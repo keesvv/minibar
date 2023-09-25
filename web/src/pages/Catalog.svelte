c26d12fda790baac38ed6b8ae65a728bfa880a28
Kees van Voorthuizen <me@keesvv.nl>
2023-09-25 22:06:34 +0200

    feat: drink water!

--- web/src/pages/Catalog.svelte
+++ web/src/pages/Catalog.svelte
@@ -2,9 +2,15 @@
   import { stock } from "../modules/beverage";
   import BeverageButton from "../lib/BeverageButton.svelte";
   import { Link } from "svelte-navigator";
+  import IconWater from "~icons/mdi/water";
+  import { _ } from "svelte-i18n";
 </script>
 
 <section class="catalog grid md:grid-flow-col gap-3">
+  <button class="btn-alt flex gap-2 items-center">
+    <IconWater />
+    <span>{$_("water")}</span>
+  </button>
   {#each $stock.filter((b) => b.capabilities.length) as beverage}
     <Link to="/details/{beverage.id}">
       <BeverageButton {beverage} />
