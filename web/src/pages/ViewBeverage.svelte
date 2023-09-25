430ad2bf78d49d9a1bac907b4796e42629c0ec0c
Kees van Voorthuizen <me@keesvv.nl>
2023-09-25 21:13:55 +0200

    fix: reactivity issue

--- web/src/pages/ViewBeverage.svelte
+++ web/src/pages/ViewBeverage.svelte
@@ -4,42 +4,43 @@
   import { stock, IconShot, IconBottle } from "../modules/beverage";
   import BeverageDetails from "../lib/BeverageDetails.svelte";
   import BeverageButton from "../lib/BeverageButton.svelte";
-  import type { SvelteComponent } from "svelte";
   import { P, match } from "ts-pattern";
 
   const params = useParams();
 
-  let beverage = $stock.find((b) => b.id === $params.id);
-  let softdrinks = $stock.filter((s) => s.metadata.category === "softdrink");
+  $: beverage = $stock.find((b) => b.id === $params.id);
+  $: softdrinks = $stock.filter((s) => s.metadata.category === "softdrink");
 
-  let shownCapabilities = beverage.capabilities.filter((c) =>
-    ["unit", "shot"].includes(c)
-  );
+  $: shownCapabilities =
+    beverage?.capabilities.filter((c) => ["unit", "shot"].includes(c)) || [];
 
-  let capabilityMeta = shownCapabilities.map((cap) => {
-    let caption = match([beverage, cap])
-      .with(
-        [{ metadata: { packaging: P.string.and(P.select()) } }, "unit"],
-        (pkg) => $_(`beverage.packaging.${pkg}`)
-      )
-      .otherwise(() => $_(`beverage.capability.${cap}`));
+  $: capabilityMeta =
+    shownCapabilities?.map((cap) => {
+      let caption = match([beverage, cap])
+        .with(
+          [{ metadata: { packaging: P.string.and(P.select()) } }, "unit"],
+          (pkg) => $_(`beverage.packaging.${pkg}`)
+        )
+        .otherwise(() => $_(`beverage.capability.${cap}`));
 
-    let icon = match([beverage, cap])
-      .with([P._, "shot"], () => IconShot)
-      .with([{ metadata: { packaging: "bottle" } }, P._], () => IconBottle)
-      .otherwise(() => null);
+      let icon = match([beverage, cap])
+        .with([P._, "shot"], () => IconShot)
+        .with([{ metadata: { packaging: "bottle" } }, P._], () => IconBottle)
+        .otherwise(() => null);
 
-    return {
-      id: cap,
-      caption,
-      icon,
-    };
-  });
+      return {
+        id: cap,
+        caption,
+        icon,
+      };
+    }) ?? [];
 </script>
 
 <section class="beverage-details">
   <div class="text-2xl mb-8">
-    <BeverageDetails {beverage} />
+    {#if beverage}
+      <BeverageDetails {beverage} />
+    {/if}
   </div>
   <div class="capabilities grid gap-3">
     {#each capabilityMeta as capability}
@@ -51,7 +52,7 @@
       </button>
     {/each}
   </div>
-  {#if beverage.capabilities.includes("mix")}
+  {#if beverage?.capabilities.includes("mix")}
     <div class="mixWith mt-3 grid gap-3">
       {#each softdrinks as softdrink}
         <button>
